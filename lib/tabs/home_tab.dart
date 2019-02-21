9a7583c45a511beb4310f83b670a07ae15e3a3ff
Marcelo Silva <marcelo.silva@luizalabs.com>
2019-02-21 17:08:43 -0300

    Crating class for request firebase

---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 11, 144, 253),
+            Color.fromARGB(255, 165, 194, 253),
+            Color.fromARGB(255, 212, 224, 253)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        );
+    return Stack(
+      //Se utiliza Stack para colocar uma coisa sobrepondo outra
+      children: <Widget>[
+        _buildBodyBack(),
+        CustomScrollView(
+          //Um scrollview personalizado
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true, //Efeito de desaparecer appbar ao descer tela
+              snap: true, //Efeito de aparição de appbar ao subir tela
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text("Novidades"),
+                centerTitle: true,
+              ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data["image"],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
+                  );
+                }
+              },
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
